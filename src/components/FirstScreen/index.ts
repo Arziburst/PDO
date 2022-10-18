ebde6c2ba2409ef8b2c9f732f308232f82838943
Oleksii <oty872020@gmail.com>
2022-10-18 21:32:38 +0300

    feat: added media query 576px

--- src/components/FirstScreen/index.ts
+++ src/components/FirstScreen/index.ts
@@ -2,6 +2,7 @@ import "../../assets/img/FirstScreen.png";
 import "../../assets/img/logo.png";
 import "../../assets/img/logo-text.png";
 import "../../assets/img/search.png";
+import "../../assets/img/menu.png";
 
 export const imageContainerFunction = () => {
   console.log(">>>imageContainerFunction <<<");
--- src/components/Header/index.ts
+++ src/components/Header/index.ts
@@ -1,6 +1,7 @@
 import "../../assets/img/logo.png";
 import "../../assets/img/logo-text.png";
 import "../../assets/img/search.png";
+import "../../assets/img/menu.png";
 
 export const headerFunction = () => {
   console.log(">>>headerFunction <<<");
